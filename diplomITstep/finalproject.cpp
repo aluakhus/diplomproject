c5be075fc0ae3f9f1db7521ed1f82aef172a05fc
unknown <aluakhus@gmail.com>
2026-06-24 14:38:16 +0500

    Изменила сохранение, загрузку. Поменяла таблицу на 1-3

--- diplomITstep/finalproject.cpp
+++ diplomITstep/finalproject.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-
+#include <string>
 using namespace std;
 
 char board[3][3];
@@ -19,25 +19,48 @@ public:
     }
     void ShowInfo() {
         cout << "Имя: " << name << endl;
-        cout << "Победы: " << wins << endl;
+        cout << "Победы: " << wins << "\n" << endl;
     }
     void AddWin() {
         wins++;
     }
     void ShowStatistics() {
         double percent = (double)wins / gamesPlayed * 100;
-        cout << "Процент побед: "<< percent<< "%\n";
+        cout << "Процент побед: " << percent << "%\n" << endl;
     }
     void AddGamesPlayed() {
         gamesPlayed++;
+    }
+    string GetName()
+    {
+        return name;
+    }
+    int GetWins()
+    {
+        return wins;
+    }
+    int GetGamesPlayed() {
+        return gamesPlayed;
+    }
+    void SetName(string n)
+    {
+        name = n;
+    }
+    void SetWins(int w)
+    {
+        wins = w;
+    }
+    void SetGamesPlayed(int g)
+    {
+        gamesPlayed = g;
     }
 };
 
 void InitBoard()
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 1; i < 4; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 1; j < 4; j++)
         {
             board[i][j] = '-';
         }
@@ -46,8 +69,8 @@ void InitBoard()
 
 void PrintBoard()
 {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
+    for (int i = 1; i < 4; i++) {
+        for (int j = 1; j < 4; j++) {
             cout << board[i][j] << " ";
         }
         cout << endl;
@@ -56,33 +79,33 @@ void PrintBoard()
 
 bool checkWin(char p) {
     // Строки
-    for (int i = 0; i < 3; i++) {
-        if (board[i][0] == p &&
-            board[i][1] == p &&
-            board[i][2] == p) {
+    for (int i = 1; i < 4; i++) {
+        if (board[i][1] == p &&
+            board[i][2] == p &&
+            board[i][3] == p) {
             return true;
         }
     }
 
     // Столбцы
-    for (int j = 0; j < 3; j++) {
-        if (board[0][j] == p &&
-            board[1][j] == p &&
-            board[2][j] == p) {
+    for (int j = 1; j < 4; j++) {
+        if (board[1][j] == p &&
+            board[2][j] == p &&
+            board[3][j] == p) {
             return true;
         }
     }
 
     // Диагонали
-    if (board[0][0] == p &&
-        board[1][1] == p &&
-        board[2][2] == p){
+    if (board[1][1] == p &&
+        board[2][2] == p &&
+        board[3][3] == p){
         return true;
         }
 
-    if (board[0][2] == p &&
-        board[1][1] == p &&
-        board[2][0] == p) {
+    if (board[1][3] == p &&
+        board[2][2] == p &&
+        board[3][1] == p) {
         return true;
     }
     return false;
@@ -90,9 +113,9 @@ bool checkWin(char p) {
 
 bool IsDraw()
 {
-    for (int i = 0; i < 3; i++)
+    for (int i = 1; i < 4; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 1; j < 4; j++)
         {
             if (board[i][j] == '-')
                 return false;
@@ -112,12 +135,12 @@ void play(Player& player)
 
         int row, col;
         cout << "Ваш ход" << endl;
-        cout << "Введите ряд(0-2): ";
+        cout << "Введите ряд(1-3): ";
         cin >> row;
-        cout << "Введите столбец(0-2): ";
+        cout << "Введите столбец(1-3): ";
         cin >> col;
 
-        if (row < 0 || row > 2 || col < 0 || col> 2 ||
+        if (row < 1 || row > 3 || col < 1 || col> 3 ||
             board[row][col] != '-')
         {
             cout << "Неправильный ход" << endl;
@@ -143,8 +166,8 @@ void play(Player& player)
         int r, c;
 
         do {
-            r = rand() % 3;
-            c = rand() % 3;
+            r = rand() % 3 + 1;
+            c = rand() % 3 + 1;
         }
         while (board[r][c] != '-');
 
@@ -160,34 +183,56 @@ void play(Player& player)
 
 void SaveGame(Player& player)
 {
-    ofstream file("saves.txt");
+    ofstream playerfile("savePlayer.txt");
 
+    playerfile << player.GetName()  << endl;
+    playerfile << player.GetWins() << endl;
+    playerfile << player.GetGamesPlayed() << endl;
+    cout << "Данные игрока сохранены\n" << endl;
+
+    playerfile.close();
+
+    ofstream gamefile("saveGame.txt");
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            file << board[i][j] << " ";
+            gamefile << board[i][j] << " ";
         }
-        file << endl;
+        gamefile << endl;
     }
 
-    file.close();
-    cout << "Игра сохранена/n" << endl;
+    gamefile.close();
+    cout << "Игра сохранена\n" << endl;
 }
 
-bool LoadGame()
+bool LoadGame(Player& player)
 {
-    ifstream file("saves.txt", ios:: app);
+    string name;
+    int games;
+    int wins;
+
+    ifstream playerfile("savePlayer.txt");
+    getline(playerfile, name);
+    playerfile >> games;
+    playerfile >> wins;
+
+    player.SetName(name);
+    player.SetWins(wins);
+    player.SetGamesPlayed(games);
+    playerfile.close();
+
+    ifstream gamefile("saveGame.txt");
 
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            file >> board[i][j];
+            gamefile >> board[i][j];
         }
     }
 
-    file.close();
+    gamefile.close();
     cout << "Игра загружена\n" << endl;
     PrintBoard();
     return true;
@@ -229,10 +274,13 @@ int main()
             SaveGame(player);
             break;
         case 5:
-            LoadGame();
+            LoadGame(player);
             break;
         case 6:
             return 0;
+        default:
+            cout << "Ошибка" << endl;
+            break;
         }
     }
 }
